47dd5b555ac5887cac20eec6cfca63bf6548fce8
codingbug671 <bushra.akram999@hotmail.com>
2022-10-27 16:49:51 +0500

    test

--- cypress/integration/todomvc-filtering.spec.js
+++ cypress/integration/todomvc-filtering.spec.js
@@ -1,43 +1,40 @@
 /// <reference types="cypress" />
-describe('filtering',()=>{
-
-    beforeEach(()=>{
-        cy.visit('http://todomvc-app-for-testing.surge.sh')
-    
-        cy.get('.new-todo').type("Clean room{enter}")
-        cy.get('.new-todo').type("Learn JavaScript{enter}")
-        cy.get('.new-todo').type("Use Cypress{enter}")
-
-        cy.get('.todo-list li:nth-child(2) .toggle').click()
-
+import {
+    navigate,
+    addTodo,
+    toggleTodo,
+    showOnlyActiveTodos,
+    showOnlyCompletedTodos,
+    showAllTodos,
+    validateNumberOfTodosShown,
+  } from '../page-objects/todo-page'
+  
+  describe('filtering', function() {
+    beforeEach(() => {
+      navigate()
+  
+      addTodo('Clean room')
+      addTodo('Learn JavaScript')
+      addTodo('Use Cypress')
+  
+      toggleTodo(1)
     })
-    
-    it('should filter "Active" todos',()=>{
-    
-    
-        cy.contains('Active').click()
-
-        cy.get('.todo-list li').should('have.length',2)
-    
+  
+    it('should filter "Active" correctly', () => {
+      showOnlyActiveTodos()
+  
+      validateNumberOfTodosShown(2)
     })
-    
-    it('should filter "Completed" todos',()=>{
-    
-    
-        cy.contains('Completed').click()
-
-        cy.get('.todo-list li').should('have.length',1)
-    
+  
+    it('should filter "Completed" correctly', () => {
+      showOnlyCompletedTodos()
+  
+      validateNumberOfTodosShown(1)
     })
-
-    it('should filter "All" todos',()=>{
-    
-    
-        cy.contains('All').click()
-
-        cy.get('.todo-list li').should('have.length',3)
-    
+  
+    it('should filter "All" correctly', () => {
+      showAllTodos()
+  
+      validateNumberOfTodosShown(3)
     })
-   
-    
-    })
+  })
